484a0c8a497b89203d910648974a183ac9085926
Cheddar Bay Biscuits <nowaczewskijack@gmail.com>
2022-05-07 10:59:30 -0400

    added window prompts to receive information from user to select type of password requested

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,15 +1,23 @@
 // Assignment code here
 
 
+
+// Starting window alert
+window.alert("Hello, welcome to the Password Generator! Please click the 'Generate Password' button, and answer the following questions to generate your password.");
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  // var password = generatePassword();
+  // var passwordText = document.querySelector("#password");
+  var windowPromptCharacters = window.prompt("How Many characters would you like your password to contain?");
+  var caseSensitive = window.prompt("would you like for your password to be case sensitive? this would include uppercase fonts in your password", "yes or no");
+  var numerical = window.prompt("Would you like for your password to contain numbers?" , "yes or no");
+  var specialCharacters = window.prompt("Would you like for your password to contain special characters? (Yes/No)", "yes or no");
+  // passwordText.value = password;
 
 }
 
